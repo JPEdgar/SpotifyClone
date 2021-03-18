97b72e1be31fb234ee91fabe61dfe886304995a6
JPEdgar <70185831+JPEdgar@users.noreply.github.com>
2021-03-17 17:28:56 -0700

    Update Login.jsx

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 
- const CLIENT_ID = "73bbbb728c664065a9d6256795922d7f"; 
+ const CLIENT_ID = ""; // gotten from spotify dev client 
  const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`;
 
 function Login() {
